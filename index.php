3343c6e4bdd4671e07da854f1b83025df0c1c87e
norzulaikha <18dit24f1995@student.ptss.edu.my>
2026-01-20 19:17:58 +0800

    pengasingan code

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Index</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php include 'biodata.php'; ?>
+
+</body>
+</html>
